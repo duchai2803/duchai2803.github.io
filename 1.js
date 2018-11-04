6184a2bfe6f48f5e27e7f2db5b147f3187ec21fe
rainbushop <duchai2803@gmail.com>
2018-11-04 12:29:49 +0700

    abc

--- 1.js
+++ 1.js
@@ -1,4 +1,22 @@
 ﻿ $(function(){
- 
+ 	$(".chuot,.kn").click(function(event) {
+ 		/* Act on the event */
+
+ 		$('body,html').animate({scrollTop:($(".fedu").offset().top)}, 700);
+ 	});
+ 	$(".sp").click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:($(".khoisp").offset().top)}, 700);
+ 	});
+
+ 	 	 
+ 	$(".tt").click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:($(".footer2").offset().top)}, 700);
+ 	});
+  	$(".gt").click(function(event) {
+ 		/* Act on the event */
+ 		$('body,html').animate({scrollTop:($(".menutop").offset().top)}, 700);
+ 	});
 })  
  
